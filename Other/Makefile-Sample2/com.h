0e7b7d6e1e8ad699dacbdeb2e85519f853a8f73a
JCFF <juancfoofn@yahoo.com>
2024-01-14 10:01:32 +0100

    .c and .h files but empty Makefile for start to try

---
+++ Other/Makefile-Sample2/com.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   com.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfidalgo <jfidalgo@student.42barcel.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 09:29:15 by jfidalgo          #+#    #+#             */
+/*   Updated: 2024/01/14 09:37:41 by jfidalgo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COM_H
+# define COM_H
+
+# define COM_VALUE 100
+
+int	start_com(void);
+int	stop_com(void);
+int	send_message(char *msg);
+
+#endif
