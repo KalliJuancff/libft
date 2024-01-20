af5e7775d7ee6881c947604f5e08052158c74ce6
JCFF <juancfoofn@yahoo.com>
2024-01-20 10:14:51 +0100

    Update main.c adding output of C standard functions too and separating functions with empty lines

--- main.c
+++ main.c
@@ -6,24 +6,30 @@
 /*   By: jfidalgo <jfidalgo@student.42barcel.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 17:01:40 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/01/18 20:55:58 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/01/20 10:11:32 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <ctype.h>
+
+void	write_empty_line()
+{
+	printf("\n");
+}
 
 void	test_isalpha(int c)
 {
-	printf("Is alpha '%c'?: %d\n", c, ft_isalpha(c));
+	printf("Is alpha '%c'?: [FT] %d; [C] %d\n", c, ft_isalpha(c), isalpha(c));
 }
 
 void	test_isdigit(int c)
 {
-	printf("Is digit '%c'?: %d\n", c, ft_isdigit(c));
+	printf("Is digit '%c'?: [FT] %d; [C] %d\n", c, ft_isdigit(c), isdigit(c));
 }
 
-int	main(void)
+void	test_ft_isalpha()
 {
 	test_isalpha('A');
 	test_isalpha('Z');
@@ -33,10 +39,21 @@ int	main(void)
 	test_isalpha('Z' + 1);
 	test_isalpha('a' - 1);
 	test_isalpha('z' + 1);
+	write_empty_line();
+}
+
+void	test_ft_isdigit()
+{
 	test_isdigit('0');
 	test_isdigit('9');
 	test_isdigit('A');
 	test_isdigit('z');
-	return (0);
+	write_empty_line();
 }
 
+int	main(void)
+{
+	test_ft_isalpha();
+	test_ft_isdigit();
+	return (0);
+}
