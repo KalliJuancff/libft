c9585f459cd1a52dd6c40e8efbfa395c3c2b53ba
JCFF <juancfoofn@yahoo.com>
2024-01-20 21:57:05 +0100

    'libft.h' didn't pass the norminette

--- libft.h
+++ libft.h
@@ -6,21 +6,21 @@
 /*   By: jfidalgo <jfidalgo@student.42barcel.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 16:59:03 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/01/20 21:08:51 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/01/20 21:54:46 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stddef.h>
+# include <stddef.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
-int	ft_toupper(int c);
+int		ft_toupper(int c);
 
 #endif
