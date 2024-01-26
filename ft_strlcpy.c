c62a5bdf0268aaa934f3f80fed6077b14b849f16
Juan Carlos Fidalgo Fernandez <jfidalgo@car7s1.42barcelona.com>
2024-01-26 18:06:55 +0100

    Remove 'restrict' from 'dst' and 'src' parameters of 'ft_strlcpy'

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 18:55:34 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/01/25 20:24:01 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/01/26 17:54:44 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
@@ -20,7 +20,7 @@ size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize
 	while (i < (dstsize - 1))
 	{
 		dst[i] = src[i];
-		i++;	
+		i++;
 	}
 	dst[i] = '\0';
 	return (i);
