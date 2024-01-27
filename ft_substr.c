7cba70f4f85c78131dbd7bbb1be674b13494eb31
JC <juancfoofn@yahoo.com>
2024-01-27 11:17:20 +0100

    Function 'ft_substr' finished: remove #include's not already used, add 'static' to function 'get_min_value' and add corner case when 'start >= ft_strlen(s)'

--- ft_substr.c
+++ ft_substr.c
@@ -6,16 +6,14 @@
 /*   By: jfidalgo <jfidalgo@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 18:20:21 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/01/26 18:20:53 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/01/27 11:15:29 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "libft.h"
 
-size_t	get_min_value(size_t value1, size_t value2)
+static size_t	get_min_value(size_t value1, size_t value2)
 {
 	if (value1 < value2)
 		return (value1);
@@ -28,6 +26,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	num_bytes;
 	size_t	i;
 
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
 	num_bytes = get_min_value(ft_strlen(s + start), len);
 	result = malloc(num_bytes + 1);
 	if (!result)
