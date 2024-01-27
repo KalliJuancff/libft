b3419da38af5772f63450777fabe146c9cc8b845
JC <juancfoofn@yahoo.com>
2024-01-27 18:09:06 +0100

    Updated 'ft_memcmp', returning '*s1 - *s2' instead of -1 or +1

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jfidalgo <jfidalgo@student.42barcel.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 16:43:25 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/01/22 17:15:37 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/01/27 17:56:32 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*p1;
 	unsigned char	*p2;
 
-	p1 = (unsigned char *)s1;
-	p2 = (unsigned char *)s2;
+	p1 = (unsigned char *) s1;
+	p2 = (unsigned char *) s2;
 	while (n > 0)
 	{
-		if (*p1 < *p2)
-			return (-1);
-		if (*p1 > *p2)
-			return (1);
+		if (*p1 != *p2)
+			return (*p1 - *p2);
 		p1++;
 		p2++;
 		n--;
