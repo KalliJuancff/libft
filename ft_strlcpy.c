a63cc08386eb2b3afd3c11394fd5dd9811ecbe86
JC <juancfoofn@yahoo.com>
2024-01-28 11:21:36 +0100

    Updated 'ft_strlcpy', returning strlen when 'dtsize == 0' and in happy path. Also add conditional to stop when null char found in while

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,24 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 18:55:34 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/01/26 17:54:44 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/01/28 11:15:23 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
+	if (dstsize == 0)
+		return (ft_strlen(src));
 	i = 0;
-	while (i < (dstsize - 1))
+	while (src[i] != '\0' && i < (dstsize - 1))
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	return (i);
+	return (ft_strlen(src));
 }
